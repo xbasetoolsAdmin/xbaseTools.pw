f78b16e455c0d92210e45ba0ccbe6bea67f26679
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-06-23 02:01:27 +0100

    Update check2cp.php

--- check2cp.php
+++ check2cp.php
@@ -1,58 +1,101 @@
 <?php
+ob_start();
+error_reporting(0);
 session_start();
 date_default_timezone_set('UTC');
-include "includes/config.php";
+include "../includes/config.php";
 
 if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){
    header("location: ../");
    exit();
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-
-function curl_get_contents($url)
-{
-  $curl = curl_init($url);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-  curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($curl, CURLOPT_TIMEOUT, 10); //timeout in seconds
-  $data = curl_exec($curl);
-  curl_close($curl);
-  return $data;
+$uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+?>
+<?php
+preg_match_all("/(\n)/", $_POST['text'], $matches);
+    $total_lines = count($matches[0]) + 1;
+	if ($total_lines > 30) { echo "limit reached"; } else { 
+function ambilKata($param, $kata1, $kata2){
+    if(strpos($param, $kata1) === FALSE) return FALSE;
+    if(strpos($param, $kata2) === FALSE) return FALSE;
+    $start = strpos($param, $kata1) + strlen($kata1);
+    $end = strpos($param, $kata2, $start);
+    $return = substr($param, $start, $end - $start);
+    return $return;
 }
-		$id = mysqli_real_escape_string($dbcon, $_GET['id']);
 
-$sql = "SELECT * FROM cpanels WHERE id=$id";
+ $date = date('d/m/Y H:i:s a');
+  $uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+$site = explode("\n", $_POST['inputs']);
+$price = mysqli_real_escape_string($dbcon, $_POST['price']);
+
+
+
+		$url = $_POST["cpanel_host"];
+		$login = $_POST["cpanel_login"];
+		$pass = $_POST["cpanel_pass"];
+		$k = "$url|$login|$pass";
 		
-$query = mysqli_query($dbcon, $sql);
-
-	function srl($item)
-		{
-		$item0 = $item;
-		$item1 = rtrim($item0);
-		$item2 = ltrim($item1);
-		return $item2;
-		} 
-while ($row = mysqli_fetch_array($query))
-$serverurl = $row['url'];
-		$d = explode("|", $serverurl);
-		$url = srl($d[0]);
-		$login = srl($d[1]);
-		$pass = srl($d[2]);
-			$o = parse_url($url, PHP_URL_HOST);
+				$o = parse_url($url, PHP_URL_HOST);
 		$cp1 = "$o";
-		$urltoapi = "http://xbasetools.xyz/index.html/apicheckcp.php?cp12=$cp1&login=$login&pass=".rawurlencode($pass)."";
-$urltoapi2 = curl_get_contents($urltoapi);
+	
+ $qq = @mysqli_query($dbcon, "SELECT * FROM cpanels") or die("error here");
+
+/* Attempt MySQL server dbconection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+
+
+	if(isset($_POST['start']) and $_POST['start'] == "work"){
+		      if ($price == 0)
+{
+	echo htmlspecialchars($ov)."Price is not valid!";
+} 
+      else if (preg_match('/[^0-9]/', $price)) {
+	 			$ov = parse_url($k, PHP_URL_HOST);
+
+	echo htmlspecialchars($ov)."Price is not valid!";
+} else {
+		$check="SELECT * FROM cpanels WHERE url = '$k'";
+$rs = mysqli_query($dbcon,$check);
+$data = mysqli_fetch_array($rs, MYSQLI_NUM);
+if($data[0] > 1) {
+ while($row = mysqli_fetch_assoc($qq)){
+     $st = $row['url'];
+	 			$ov = parse_url($k, PHP_URL_HOST);
+
+  			 if (preg_match("#$ov#", $st)) { 
+			 } 
+    }
+
+    echo "Already Added !";
+} else {
+	
+		ini_set('max_execution_time', 10);
+$urltoapi = "https://xbasetools.xyz/index.html/apicheckcp.php?cp12=$cp1&login=$login&pass=".rawurlencode($pass)."";
+$urltoapi2 = file_get_contents($urltoapi);
 	if (preg_match('#CP Work#', $urltoapi2))
 		{
-	    echo "<span class='label label-success'>Working</span>";
-		return true;
-	} else { 
-	    echo "<span class='label label-danger'>Bad!</span>";
+$o = parse_url($url, PHP_URL_HOST);
+		$oip = gethostbyname($o);
+		$iptohosting = "https://api.ipgeolocation.io/ipgeo?apiKey=b4692370f68e4c398039408226e6f99f&ip=$oip&fields=isp";
+		$creatorhosting = file_get_contents($iptohosting);
+		$hostingg = ambilkata($creatorhosting, '{"ip":"'.$oip.'","isp":"', '"}');
+		$iptolocation = "http://api.ipstack.com/$o?access_key=f991d31642a29f8a8197b57ef76f167b&fields=country_name";
+		$creatorlocation = file_get_contents($iptolocation);
+		$country = ambilkata($creatorlocation, '{"country_name":"', '"}');
+		$date = date('Y/m/d h:i:s');
+		$uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+		mysqli_query($dbcon, "INSERT INTO cpanels
+  (acctype,country,infos,url,price,sold,sto,dateofsold,resseller,date,reported)
+  VALUES
+  ('cpanel','$country','$hostingg','$k','$price','0','',null,'$uid','$date','')") or die(mysqli_error($dbcon));
+    echo htmlspecialchars($o)." .. Added Succesfully";
+   
+				} else {  
+    echo "Not Working !";
+		}
+	} 
 
-	$sql = "UPDATE cpanels SET sold='deleted' WHERE id='$id'";
-$query = mysqli_query($dbcon, $sql);
-	}
-	
-	?>
+	} }  } 
+?>
